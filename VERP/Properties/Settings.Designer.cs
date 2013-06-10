37696e3114f1f65a2d518bf079f7820328b9e9bd
caioketo <caioketo@live.com>
2013-06-10 16:53:33 -0300

    c

--- VERP/Properties/Settings.Designer.cs
+++ VERP/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace VERP.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool FirstRun {
             get {
                 return ((bool)(this["FirstRun"]));
